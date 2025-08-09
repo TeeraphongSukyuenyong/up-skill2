538018fd8d6f3bb943c86ca2c3a8475fefe526bc
TeeraphongSukyuenyong <160911829+TeeraphongSukyuenyong@users.noreply.github.com>
2025-08-09 16:19:37 +0700

    InvoiceandCustomer

---
+++ Customer.java
@@ -0,0 +1,58 @@
+public class Customer {
+    private int id;
+    private String name;
+    private DiscountPolicy discountPolicy;
+
+    /*
+     * Constructor for create Customer object
+     * * @param id ไม่เป็นค่าติดลบ *
+     * 
+     * @param name ไม่เป็นค่าว่าง *
+     * 
+     * @param discount ส่วนลด(%) ต้องอยู่ระหว่าง 0-100 *
+     * 
+     * @throws IllegalArgumentException หาก @params ไม่ถูกต้อง
+     */
+    public Customer(int id, String name, DiscountPolicy discountPolicy) {
+        if (id < 0) {
+            throw new IllegalArgumentException("Customer ID must be non-negative.");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Customer Name must not be null or blank.");
+        }
+        if (discountPolicy == null) {
+            throw new IllegalArgumentException("Customer must have a discount policy");
+        }
+
+        this.id = id;
+        this.name = name;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DiscountPolicy getDiscountPolicy() {
+        return this.discountPolicy;
+    }
+
+    public void setDiscount(DiscountPolicy policy) {
+        if (policy == null) {
+            throw new IllegalArgumentException("Customer must have a discount policy");
+        }
+        this.discountPolicy = policy;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name= " + name +
+                ", discountPolicy= " + discountPolicy + "}";
+    }
+}
---
+++ DiscountPolicy.java
@@ -0,0 +1,8 @@
+public interface DiscountPolicy {
+
+    /*
+     * @param amount The original amount
+     * @return The amount after the discount is applied.
+     */
+    double applyDiscount(double amount);    
+}
